6599611bcbbb659a696c323e3bd776d9fc43ffc0
Daniel Condie <daniel.condie@4mation.com.au>
2016-07-14 13:17:16 +1000

    removed the sample dog resource and associated code

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -19,10 +19,3 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
-
-$factory->define(App\Dog::class, function (Faker\Generator $faker) {
-    return [
-        'name' => $faker->name,
-        'age' => $faker->randomDigit,
-    ];
-});
